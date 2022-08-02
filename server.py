cb6fbaa2957a943545b0e07241006c967a32d90f
Sanlys <slysakermoen@gmail.com>
2022-08-02 20:54:36 +0200

    Removed commented code

--- server.py
+++ server.py
@@ -44,38 +44,6 @@ def func():
 
 @app.route("/")
 def hello_world():
-    """
-    primaryDiskName='/dev/nvme0n1p2'
-    diskAmount = 3
-
-    toReturn = {}
-    toReturn['cpu_name'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/cpu_name.sh'").read()[:-1]
-    toReturn['cpu_usage'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/cpu_usage.sh'").read()[:-2]
-    toReturn['mem_min'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/mem_min.sh'").read()[:-1]
-    toReturn['mem_max'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/mem_max.sh'").read()[:-1]
-    toReturn['mem_used_mb'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/mem_used_mb.sh'").read()[:-1]
-    toReturn['mem_used_percentage'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/mem_used_percentage.sh'").read()[:-1]
-    toReturn['mem_free'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/mem_free.sh'").read()[:-1]
-    toReturn['primary_disk'] = {
-        "name": primaryDiskName,
-        "total_mb": os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/primary_disk_total_mb.sh'").read()[:-1],
-        "use_percentage": os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/primary_disk_use_percentage.sh'").read()[:-2],
-        "use_mb": os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/primary_disk_use_mb.sh'").read()[:-1]
-    }
-    toReturn['disks'] = []
-    for i in range(0,diskAmount):
-        toReturn['disks'].append({"name": os.popen(f"sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/disk_name.sh {i+2}'").read()[:-1], "total_mb": os.popen(f"sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/disk_total_mb.sh {i+2}'").read()[:-1], "use_percentage": os.popen(f"sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/disk_use_percentage.sh {i+2}'").read()[:-2], "use_mb": os.popen(f"sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/disk_use_mb.sh {i+2}'").read()[:-1]})
-
-    toReturn['kernel'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/kernel.sh'").read()[:-1]
-    toReturn['hostname'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/hostname.sh'").read()[:-1]
-    #toReturn['update_status'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/update_status.sh'").read()[:-1]
-    toReturn['update_status'] = "no"
-    toReturn['ip_addr'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/ip_addr.sh'").read()[:-1]
-    toReturn['uptime'] = os.popen("sshpass -pSander210404! ssh sander@desktop '/home/sander/monitoringscripts/uptime.sh'").read()[:-1]
-    toReturn['timestamp'] = datetime.now(timezone(timedelta(hours=2))).strftime("%H:%M:%S")
-    return jsonify(toReturn)
-    """
-
     args = request.args
     deviceID = args.get('deviceID')
 
